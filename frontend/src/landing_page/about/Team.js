73634eeec978b1a4805c0dd331df37f4da9d3531
Shivam Kumar Patel <151195034+ShivamPatel148@users.noreply.github.com>
2025-09-24 21:03:29 +0530

    update files

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -28,8 +28,8 @@ function Team() {
           </p>
           <p>Playing basketball is his zen.</p>
           <p className="Aboutus-Links">
-            Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> /{" "}
-            <a href="">Twitter</a>
+            Connect on <a href="#">Homepage</a> / <a href="#">TradingQnA</a> /{" "}
+            <a href="#">Twitter</a>
           </p>
       </div>
     </div>
