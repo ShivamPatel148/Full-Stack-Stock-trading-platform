ed0cf604bc2830aebe42611c6f1dd1dca7fa131a
Shivam Kumar Patel <151195034+ShivamPatel148@users.noreply.github.com>
2025-09-26 14:44:06 +0530

    update files

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <section className="container-fluid" id="supportHero">
       <div className="p-5 mt-5 mb-5  " id="supportWrapper">
         <h4>Support Portal</h4>
-        <a style={{color:"white"}} href="">Track Tickets</a>
+        <a style={{color:"white"}} href="/">Track Tickets</a>
       </div>
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
